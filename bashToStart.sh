d4d20df7eb867d430508b339c7ae0d81bb1ce051
shafiquerashid <shafique.a.rashid@gmail.com>
2016-02-02 16:12:10 -0800

    Now disallowing yes/no increment if vote already revieved from email address for corresponding poll.

--- bashToStart.sh
+++ bashToStart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-dropdb wayd
 createdb wayd
 osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' 
 osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
@@ -13,4 +12,3 @@ osascript -e 'tell application "Terminal" to do script "nodemon /Users/shafiquer
 osascript -e 'tell application "Terminal" to do script "nodemon /Users/shafiquerashid/HR/wayd/Server/Workers/jobserver.js" in tab 4 of window 1'
 osascript -e 'tell application "Terminal" to do script "node /Users/shafiquerashid/HR/wayd/Server/Workers/jobqueue.js" in tab 5 of window 1'
 osascript -e 'tell application "Terminal" to do script "open /Users/shafiquerashid/HR/wayd/wayd.xcworkspace" in tab 6 of window 1'
-
